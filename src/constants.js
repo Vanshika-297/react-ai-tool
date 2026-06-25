df7674660f5fc207fef65aea62199fb079b983b8
vanshika <vk340649@gmail.com>
2026-06-25 13:05:49 +0530

    secure gemini api key

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyAHMh_wLRmXjatCPrll5XjlfWfjNi81Peg" 
+export const URL =
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${import.meta.env.VITE_GEMINI_API_KEY}`;
